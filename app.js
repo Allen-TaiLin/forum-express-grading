0b940b84f5ba1faf57d19d595f150991b92f8349
Allen-TaiLin <skygary2000@gmail.com>
2021-02-02 00:05:21 +0800

    Add restaurant index page

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const exphbs = require('express-handlebars')  // 引入 handlebars
 const app = express()
 const port = 3000
 
-app.engine('handlebars', exphbs())  // Handlebars 註冊樣板引擎
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))  // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars')  // 設定使用 Handlebars 做為樣板引擎
 
 
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening on http://localhost:${port}`)
 })
 
+// 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
 require('./routes')(app)
 
 module.exports = app
