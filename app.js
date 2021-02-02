e08615f7a3449bbb4898adc08970a681779376c1
Allen-TaiLin <skygary2000@gmail.com>
2021-02-03 00:21:04 +0800

    Add userController and signup page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')  // 引入 handlebars
+const bodyParser = require('body-parser')
 const db = require('./models')  // 引入資料庫
 const app = express()
 const port = 3000
@@ -7,6 +8,7 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))  // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars')  // 設定使用 Handlebars 做為樣板引擎
 
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.listen(port, () => {
