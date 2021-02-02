f3a232a4b0e01491ef5d27cbbc1fff49e1ebcd90
Allen-TaiLin <skygary2000@gmail.com>
2021-02-03 01:07:04 +0800

    Install express-session connect-flash & Confirm user password and unique email

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const exphbs = require('express-handlebars')  // 引入 handlebars
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const flash = require('connect-flash')
 const db = require('./models')  // 引入資料庫
 const app = express()
 const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))  // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars')  // 設定使用 Handlebars 做為樣板引擎
+app.use(bodyParser.urlencoded({ extended: true }))  // setup bodyParser
+// setup session and flash
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
 
-app.use(bodyParser.urlencoded({ extended: true }))
+// 把 req.flash 放到 res.locals 裡面
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 
 app.listen(port, () => {
