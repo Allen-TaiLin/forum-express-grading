8bf309c5871d5eb19112fbda7057ed6323180976
Allen-TaiLin <skygary2000@gmail.com>
2021-02-03 12:29:59 +0800

    Add isAdmin to users & Add authentication before rendering pages

---
+++ migrations/20210203033132-add-isAdmin-to-users.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { sequelize } = require("../models");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Users', 'isAdmin', {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+    })
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Users', 'isAdmin')
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
