83b735da6732868566ce7da4f07e2fa61486208a
Allen-TaiLin <skygary2000@gmail.com>
2021-02-08 21:50:17 +0800

    Update category select in edit page using ifCond-helper

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ const db = require('./models')  // 引入資料庫
 const app = express()
 const port = process.env.PORT || 3000
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))  // Handlebars 註冊樣板引擎
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  helpers: require('./config/handlebars-helper')
+}))  // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars')  // 設定使用 Handlebars 做為樣板引擎
 app.use(bodyParser.urlencoded({ extended: true }))  // setup bodyParser
 app.use(methodOverride('_method'))
