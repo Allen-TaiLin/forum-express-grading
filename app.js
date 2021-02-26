42034493fe5aea84a35b4be1d797f042acbc1078
Allen-TaiLin <skygary2000@gmail.com>
2021-02-26 12:24:50 +0800

    feat: POST /api/admin/restaurants

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.engine('handlebars', exphbs({
 }))  // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars')  // 設定使用 Handlebars 做為樣板引擎
 app.use(bodyParser.urlencoded({ extended: true }))  // setup bodyParser
+app.use(bodyParser.json())
 app.use(methodOverride('_method'))
 // setup session and flash
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
--- routes/apis.js
+++ routes/apis.js
@@ -4,12 +4,18 @@ const router = express.Router()
 const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController')
 
+// 引入 multer 並設定上傳資料夾 
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
+
 // 全部餐廳資料
 router.get('/admin/restaurants', adminController.getRestaurants)
 // 查詢一筆資料
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 // 瀏覽分類
 router.get('/admin/categories', categoryController.getCategories)
+// 新增餐廳
+router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
 // 刪除餐廳
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 
