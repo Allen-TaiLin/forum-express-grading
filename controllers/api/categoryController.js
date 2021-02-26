90327d2d35850d3f590c490d0408e74d4682a534
Allen-TaiLin <skygary2000@gmail.com>
2021-02-26 13:21:58 +0800

    feat: POST /api/admin/categories

--- controllers/api/categoryController.js
+++ controllers/api/categoryController.js
@@ -10,6 +10,12 @@ const categoryController = {
     })
   },
 
+  postCategory: (req, res) => {
+    categoryService.postCategory(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+
   putCategory: (req, res) => {
     categoryService.putCategory(req, res, (data) => {
       return res.json(data)
