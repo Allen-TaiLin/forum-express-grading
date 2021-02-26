dbaee5bcaee283a7d985477f340a0f1becf9bb1c
Allen-TaiLin <skygary2000@gmail.com>
2021-02-26 13:35:19 +0800

    feat: DELETE /api/admin/categories/:id

--- routes/apis.js
+++ routes/apis.js
@@ -14,15 +14,18 @@ router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 // 修改表單(個別餐廳)
 router.put('/admin/restaurants/:id', upload.single('image'), adminController.putRestaurant)
+// 新增餐廳
+router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
+// 刪除餐廳
+router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
+
 // 瀏覽餐聽分類
 router.get('/admin/categories', categoryController.getCategories)
 // 新增餐聽分類
 router.post('/admin/categories', categoryController.postCategory)
 // 修改餐聽分類
 router.put('/admin/categories/:id', categoryController.putCategory)
-// 新增餐廳
-router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
-// 刪除餐廳
-router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
+// 刪除餐聽分類
+router.delete('/admin/categories/:id', categoryController.deleteCategory)
 
 module.exports = router
